0dc70a2cbe101cdd7037ed8d5ded594bcffaa67d
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-05-07 07:06:34 +0100

    Autoload bot files

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,5 +1,9 @@
 require 'facebook/messenger'
 
+if Rails.env.production?
+  Dir["#{Rails.root}/app/bots/**/*.rb"].each { |file| require file }
+end
+
 Facebook::Messenger.configure do |config|
   config.access_token = ENV['ACCESS_TOKEN']
   config.verify_token = ENV['VERIFY_TOKEN']
